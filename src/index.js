a201a907bf7e5370788047c2ffad08f7623bda7d
Xiaosong Gao <gaoxiaosong06@gmail.com>
2019-01-20 02:14:28 +0800

    添加评论插件的代码。

---
+++ src/index.js
@@ -0,0 +1,44 @@
+import styles from './styles.css';
+
+function docsifyFoldComment(hook, vm) {
+    hook.doneEach(function () {
+        const nodes = document.querySelector('#main').childNodes;
+        nodes.forEach(function (val, i) {
+            // filter not p+pre[data-lang="comment"]
+            if (i === 0) return;
+            if (val.nodeName !== 'PRE') return;
+            if (val.getAttribute('data-lang') !== 'comment') return;
+            if (nodes[i - 1].nodeName !== 'P') return;
+            // Hide Code
+            val.style.display = 'none';
+            // Add button
+            const img = document.createElement('img');
+            img.src = './right.jpg';
+            img.classList.add('docsify-fold-comment-button');
+            nodes[i - 1].appendChild(img);
+            // Set parent position
+            nodes[i - 1].style.position = 'relative';
+        });
+    });
+    hook.mounted(function () {
+        const listenerHost = document.querySelector('.content');
+        listenerHost.addEventListener('click', function (evt) {
+            const isCommentButton = evt.target.classList.contains('docsify-fold-comment-button');
+            if (isCommentButton) {
+                const button = evt.target;
+                const codeNode = button.parentNode.nextSibling;
+                const isHidden = codeNode.style.display === 'none';
+                if (isHidden) {
+                    button.src = './down.jpg';
+                    codeNode.style.display = 'block';
+                } else {
+                    button.src = './right.jpg';
+                    codeNode.style.display = 'none';
+                }
+            }
+        });
+    });
+}
+
+window.$docsify = window.$docsify || {};
+window.$docsify.plugins = [docsifyFoldComment].concat(window.$docsify.plugins || []);
